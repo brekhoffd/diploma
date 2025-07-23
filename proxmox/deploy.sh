3d0d1e2befed2d842676df6e99c639dac646d7b0
brekhoffd <denys.brekhov@gmail.com>
2025-07-23 11:21:07 +0300

    Deploy and run script on Proxmox VE

---
+++ proxmox/deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Налаштовуємо підключення
+REMOTE_USER="root"
+REMOTE_HOST="192.168.88.1"
+
+# Вказуємо шляхи
+REMOTE_SCRIPT_PATH="/tmp/ubuntu_template.sh"
+LOCAL_SCRIPT="./ubuntu_template.sh"
+
+# Копіюємо скрипт на сервер
+scp "$LOCAL_SCRIPT" "$REMOTE_USER@$REMOTE_HOST:$REMOTE_SCRIPT_PATH"
+
+# Підключаємось і запускаємо скрипт
+ssh "$REMOTE_USER@$REMOTE_HOST" "chmod +x $REMOTE_SCRIPT_PATH && $REMOTE_SCRIPT_PATH && rm -f $REMOTE_SCRIPT_PATH"

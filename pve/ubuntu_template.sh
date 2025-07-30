37bc743604d9f4897bbe0950aca82a3c4bd4a73f
brekhoffd <denys.brekhov@gmail.com>
2025-07-30 19:33:05 +0300

    Update ubuntu_template.sh

--- pve/ubuntu_template.sh
+++ pve/ubuntu_template.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo Створюємо шаблон віртуальної машини...
+echo "Створюємо шаблон віртуальної машини..."
 
 # Cloud Image
 CLOUD_IMAGE=noble-server-cloudimg-amd64.img
@@ -13,11 +13,11 @@ VM_CORES=1
 VM_STORAGE=raid-zfs
 
 # Завантажуємо Cloud-Image
-echo Завантажуємо Cloud-Image...
+echo "Завантажуємо Cloud-Image..."
 wget https://cloud-images.ubuntu.com/noble/current/${CLOUD_IMAGE}
 
 # Створюємо віртуальну машину
-echo Створюємо віртуальну машину...
+echo "Створюємо віртуальну машину..."
 qm create ${VM_ID} \
   --name ${VM_NAME} \
   --memory ${VM_MEM} \
@@ -30,15 +30,15 @@ qm create ${VM_ID} \
   --agent enabled=1
 
 # Імпортуємо диск
-echo Імпортуємо диск...
+echo "Імпортуємо диск..."
 qm importdisk ${VM_ID} ${CLOUD_IMAGE} ${VM_STORAGE}
 
 # Очікуємо створення ZVOL-пристрою після імпорту диска
-echo Очікуємо створення ZVOL-пристрою після імпорту диска...
+echo "Очікуємо створення ZVOL-пристрою після імпорту диска..."
 sleep 5
 
 # Перевіряємо наявність ZVOL-пристрою
-echo Перевіряємо наявність ZVOL-пристрою...
+echo "Перевіряємо наявність ZVOL-пристрою..."
 ZVOL_PATH="/dev/zvol/${VM_STORAGE}/vm-${VM_ID}-disk-0"
 echo "Перевіряємо наявність ZVOL-пристрою: ${ZVOL_PATH}..."
 for i in {1..20}; do
@@ -55,27 +55,27 @@ if [ ! -e "$ZVOL_PATH" ]; then
 fi
 
 # Призначаємо диск до SCSI контролера
-echo Призначаємо диск до SCSI контролера...
+echo "Призначаємо диск до SCSI контролера..."
 qm set ${VM_ID} --virtio0 ${VM_STORAGE}:vm-${VM_ID}-disk-0
 
 # Підключаємо cloud-init диск
-echo Підключаємо cloud-init диск...
+echo "Підключаємо cloud-init диск..."
 qm set ${VM_ID} --ide2 ${VM_STORAGE}:cloudinit
 
 # Встановлюємо тип операційної системи
-echo  Встановлюємо тип операційної системи...
+echo "Встановлюємо тип операційної системи..."
 qm set ${VM_ID} --ostype l26
 
 # Встановлюємо пріоритет завантаження
-echo Встановлюємо пріоритет завантаження...
+echo "Встановлюємо пріоритет завантаження..."
 qm set ${VM_ID} --boot c --bootdisk virtio0
 
 # Конвертуємо ВМ у шаблон
-echo Конвертуємо ВМ у шаблон...
+echo "Конвертуємо ВМ у шаблон..."
 qm template ${VM_ID}
 
 # Видаляємо раніше завантажений Cloud-Image
-echo Видаляємо раніше завантажений Cloud-Image...
+echo "Видаляємо раніше завантажений Cloud-Image..."
 rm ./${CLOUD_IMAGE}
 
-echo Шаблон створений та готовий до клонування!
+echo "Шаблон створений та готовий до клонування!"

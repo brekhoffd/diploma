4159803bddccf7f993cbc0388124078e2eeee114
brekhoffd <denys.brekhov@gmail.com>
2025-07-30 19:36:55 +0300

    Update deploy.sh

--- pve/deploy.sh
+++ pve/deploy.sh
@@ -9,7 +9,9 @@ REMOTE_SCRIPT_PATH="/tmp/ubuntu_template.sh"
 LOCAL_SCRIPT="./ubuntu_template.sh"
 
 # Копіюємо скрипт на сервер
+echo " Копіюємо скрипт на сервер..."
 scp "$LOCAL_SCRIPT" "$REMOTE_USER@$REMOTE_HOST:$REMOTE_SCRIPT_PATH"
 
 # Підключаємось і запускаємо скрипт
+echo "Підключаємось і запускаємо скрипт..."
 ssh "$REMOTE_USER@$REMOTE_HOST" "chmod +x $REMOTE_SCRIPT_PATH && $REMOTE_SCRIPT_PATH && rm -f $REMOTE_SCRIPT_PATH"

ed2b73a3bfc9189fcf37b92688f58d951a353b6f
brekhoffd <denys.brekhov@gmail.com>
2025-07-30 19:05:18 +0300

    Update ubuntu_template.sh

--- pve/ubuntu_template.sh
+++ pve/ubuntu_template.sh
@@ -12,10 +12,12 @@ VM_MEM=1024
 VM_CORES=1
 VM_STORAGE=raid-zfs
 
-# Завантажуємо Cloud Image
+# Завантажуємо Cloud-Image
+echo Завантажуємо Cloud-Image...
 wget https://cloud-images.ubuntu.com/noble/current/${CLOUD_IMAGE}
 
 # Створюємо віртуальну машину
+echo Створюємо віртуальну машину...
 qm create ${VM_ID} \
   --name ${VM_NAME} \
   --memory ${VM_MEM} \
@@ -28,40 +30,52 @@ qm create ${VM_ID} \
   --agent enabled=1
 
 # Імпортуємо диск
+echo Імпортуємо диск...
 qm importdisk ${VM_ID} ${CLOUD_IMAGE} ${VM_STORAGE}
 
 # Очікуємо створення ZVOL-пристрою після імпорту диска
+echo Очікуємо створення ZVOL-пристрою після імпорту диска...
+sleep 5
+
+# Перевіряємо наявність ZVOL-пристрою
+echo Перевіряємо наявність ZVOL-пристрою...
 ZVOL_PATH="/dev/zvol/${VM_STORAGE}/vm-${VM_ID}-disk-0"
-echo "Очікуємо створення ZVOL-пристрою: ${ZVOL_PATH}..."
+echo "Перевіряємо наявність ZVOL-пристрою: ${ZVOL_PATH}..."
 for i in {1..20}; do
   if [ -e "$ZVOL_PATH" ]; then
-    echo "ZVOL створено!"
+    echo "ZVOL знайдено!"
     break
   fi
   sleep 1
 done
 
 if [ ! -e "$ZVOL_PATH" ]; then
-  echo "Помилка: ZVOL не створено. Перевір Proxmox та ZFS."
+  echo "Помилка: ZVOL не знайдено. Перевір Proxmox та ZFS."
   exit 1
 fi
 
 # Призначаємо диск до SCSI контролера
+echo Призначаємо диск до SCSI контролера...
 qm set ${VM_ID} --virtio0 ${VM_STORAGE}:vm-${VM_ID}-disk-0
 
 # Підключаємо cloud-init диск
+echo Підключаємо cloud-init диск...
 qm set ${VM_ID} --ide2 ${VM_STORAGE}:cloudinit
 
 # Встановлюємо тип операційної системи
+echo  Встановлюємо тип операційної системи...
 qm set ${VM_ID} --ostype l26
 
 # Встановлюємо пріоритет завантаження
+echo Встановлюємо пріоритет завантаження...
 qm set ${VM_ID} --boot c --bootdisk virtio0
 
 # Конвертуємо ВМ у шаблон
+echo Конвертуємо ВМ у шаблон...
 qm template ${VM_ID}
 
-# Видаляємо раніше завантажений Cloud Image
+# Видаляємо раніше завантажений Cloud-Image
+echo Видаляємо раніше завантажений Cloud-Image...
 rm ./${CLOUD_IMAGE}
 
 echo Шаблон створений та готовий до клонування!

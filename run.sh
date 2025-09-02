671bcb5852c3bb50a567ea1437d66a02952fb658
Denys Brekhov <denys.brekhov@gmail.com>
2025-09-02 16:01:14 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,16 +5,16 @@ set -euo pipefail
 trap 'echo "Сталася помилка на рядку $LINENO"; exit 1' ERR
 
 # Цикл для запуску deploy.sh в кожній папці проекту
-for dir in aws pve; do
+for dir in ./aws ./pve; do
   echo
-  echo "Запускаю deploy.sh у папці /$dir..."
+  echo "Запускаю deploy.sh для $dir..."
   if [ -x "$dir/deploy.sh" ]; then
     (cd "$dir" && ./deploy.sh)
   else
     echo
-    echo "Файл /$dir/deploy.sh не знайдено або не має прав на виконання."
+    echo "Файл $dir/deploy.sh не знайдено або не має прав на виконання."
     exit 1
   fi
 done
 
-echo "Всі deploy.sh виконані успішно!"
+echo "Проект розгорнутий успішно!"

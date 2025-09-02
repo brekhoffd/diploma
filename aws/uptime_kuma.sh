31669aa9e3224ac03573b2aabb9494ac215d2a0f
Denys Brekhov <denys.brekhov@gmail.com>
2025-09-02 13:29:36 +0300

    Update uptime_kuma.sh

--- aws/uptime_kuma.sh
+++ aws/uptime_kuma.sh
@@ -1,23 +1,34 @@
 #!/usr/bin/env bash
 set -euo pipefail
-IFS=$'\n\t'
 
 # Обробка помилок
 trap 'echo "Сталася помилка на рядку $LINENO"; exit 1' ERR
 
+# Початкове повідомлення
+echo
+echo "Встановлення програми моніторингу Uptime Kuma!"
+
 # Оновлення системи
+echo
 echo "Оновлення системи..."
 apt update && apt full-upgrade -y
 
 # Встановлення Docker
+echo
 echo "Встановлення Docker..."
 apt install docker.io -y
 
-# Ввімкнення та запуск Docker
-echo "Ввімкнення та запуск Docker..."
-systemctl start docker
+# Запуск Docker
+echo
+echo "Запуск Docker..."
 systemctl enable docker
+systemctl start docker
 
 # Запуск контейнера Uptime Kuma
+echo
 echo "Запуск контейнера Uptime Kuma..."
 docker run -d --restart=on-failure -p 3001:3001 --name uptime-kuma brekhoffd/uptime-kuma
+
+# Вивід кінцевої інформації
+echo
+echo "Встановлення програми моніторингу Uptime Kuma завершено!"

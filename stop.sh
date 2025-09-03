ef815a80f292886b4ae54e67f78ec0bbb627c86b
Denys Brekhov <denys.brekhov@gmail.com>
2025-09-03 14:39:58 +0300

    Create stop.sh

---
+++ stop.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# Обробка помилок
+trap 'echo "Сталася помилка на рядку $LINENO"; exit 1' ERR
+
+echo
+echo "ВИДАЛЕННЯ ДИПЛОМНОГО ПРОЕКТУ!"
+
+# Цикл по папках з Terraform
+for dir in aws pve; do
+  echo
+  echo "Видалення у $dir..."
+  if [ -d "$dir" ]; then
+    (cd "$dir" && terraform destroy --auto-approve)
+  else
+    echo
+    echo "Папка $dir не знайдена."
+    exit 1
+  fi
+done
+
+echo
+echo "ДИПЛОМНИЙ ПРОЕКТ ВИДАЛЕНО УСПІШНО!"

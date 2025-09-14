88d99e1863e63bca6f050cdc16709e5aa9c90f4b
brekhoffd <denys.brekhov@gmail.com>
2025-09-14 13:28:43 +0300

    Upload 01-COSMERIA.groovy

---
+++ pve/jenkins/groovy/01-COSMERIA.groovy
@@ -0,0 +1,36 @@
+import jenkins.model.*
+import hudson.model.*
+import org.jenkinsci.plugins.workflow.job.*
+import org.jenkinsci.plugins.workflow.cps.*
+
+def jenkins = Jenkins.instance
+
+def jobName = "COSMERIA"
+def job = jenkins.getItem(jobName)
+
+if (job == null) {
+    println(">>> Creating job: ${jobName}")
+
+    def p = jenkins.createProject(WorkflowJob, jobName)
+    def cpsScmDef = new CpsScmFlowDefinition(
+        new hudson.plugins.git.GitSCM(
+            hudson.plugins.git.GitSCM.createRepoList("https://github.com/brekhoffd/diploma.git", ""),
+            [new hudson.plugins.git.BranchSpec("*/main")],
+            false,
+            [],
+            null,
+            null,
+            []
+        ),
+        "pve/jenkins/jenkinsfile"
+    )
+    cpsScmDef.setLightweight(true)
+    p.setDefinition(cpsScmDef)
+    p.save()
+
+    // 🔹 одразу запускаємо перший білд
+    println(">>> Triggering first build for ${jobName}")
+    p.scheduleBuild2(0)
+} else {
+    println(">>> Job ${jobName} already exists")
+}

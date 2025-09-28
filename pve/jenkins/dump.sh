1c26fe260bca9005286af4b2ed6a25caacc52d08
brekhoffd <denys.brekhov@gmail.com>
2025-09-28 18:12:08 +0300

    Update dump.sh

--- pve/jenkins/dump.sh
+++ pve/jenkins/dump.sh
@@ -7,8 +7,12 @@ DB_NAME="cosmeria"
 DB_USER="krot"
 DB_PASS="p%rSDj4Imds0763300sfgs7djc**dmUntdOidd3dZ_#WTi4B9Zo"
 
-# Відновлення бази даних
+# Оновлення бази даних
+echo
+echo "Оновлення бази даних..."
 PGPASSWORD="$DB_PASS" sudo docker exec -i "$CONTAINER_NAME" pg_restore --clean -U "$DB_USER" -d "$DB_NAME" < "$DUMP_FILE"
 
 # Перезапуск Docker контейнерів
+echo
+echo "Перезапуск Docker контейнерів..."
 sudo docker restart $(sudo docker ps -aq)
